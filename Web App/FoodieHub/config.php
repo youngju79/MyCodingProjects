1b345a26ddf2a9bad5f44fa48186b61724cd990d
Yong Zu Shin <yongzush@usc.edu>
2021-04-27 20:53:38 -0400

    update projects

--- Web App/FoodieHub/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$host = $url["host"];
-	$user = $url["user"];
-	$pass = $url["pass"];
-	$db = substr($url["path"], 1);
-?>
